76c5a9b83a231415a1e0e38edbf8b8451de82acb
booshja <jacob.andes@gmail.com>
2021-07-16 19:57:29 -0700

    added createAdminToken helper file, fixed test command in package.json

---
+++ helpers/tokens.test.js
@@ -0,0 +1,33 @@
+const jwt = require("jsonwebtoken");
+const { createAdminToken } = require("./tokens");
+const { SECRET_KEY } = require("../config");
+const { expectCt } = require("helmet");
+
+describe("createAdminToken", () => {
+    it("works: admin", () => {
+        const token = createAdminToken({ username: "tester", isAdmin: true });
+        const payload = jwt.verify(token, SECRET_KEY);
+        expect(payload).toEqual({
+            iat: expect.any(Number),
+            isAdmin: true,
+        });
+    });
+
+    it("works: not admin", () => {
+        const token = createAdminToken({ username: "badbad", isAdmin: false });
+        const payload = jwt.verify(token, SECRET_KEY);
+        expect(payload).toEqual({
+            iat: expect.any(Number),
+            isAdmin: false,
+        });
+    });
+
+    it("works: default no admin status", () => {
+        const token = createAdminToken({ username: "default" });
+        const payload = jwt.verify(token, SECRET_KEY);
+        expect(payload).toEqual({
+            iat: expect.any(Number),
+            isAdmin: false,
+        });
+    });
+});

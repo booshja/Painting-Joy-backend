02a8464edf9c9aa5a369cd37fdbacd88b736f8d6
booshja <jacob.andes@gmail.com>
2021-07-22 21:28:33 -0700

    refactor: changed snake_case to camelCase in igpost model

--- models/igpost.js
+++ models/igpost.js
@@ -8,57 +8,63 @@ class IGPost {
         /** Add post to db
          *
          * Accepts data
-         *      Data should be: { ig_id, caption, perm_url, image_url }
+         *      Data should be: { igId, caption, permUrl, imageUrl }
          *
-         * Returns { ig_id, caption, perm_url, image_url }
+         * Returns { igId, caption, permUrl, imageUrl }
          *
          * Throws BadRequestError if no data or data missing
          */
         // check for missing / incomplete data
         const keys = Object.keys(data);
         if (keys.length === 0) throw new BadRequestError("No data.");
-        if (!data.ig_id || !data.caption || !data.perm_url || !data.image_url)
+        if (!data.igId || !data.caption || !data.permUrl || !data.imageUrl)
             throw new BadRequestError("Missing data.");
 
         // query db to create new igpost record
         const result = await db.query(
-            `INSERT INTO igposts (ig_id, caption, perm_url, image_url)
-            VALUES ($1, $2, $3, $4)
-            RETURNING ig_id, caption, perm_url, image_url`,
-            [data.ig_id, data.caption, data.perm_url, data.image_url]
+            `INSERT INTO igposts (ig_id,
+                                caption,
+                                perm_url,
+                                image_url)
+                VALUES ($1, $2, $3, $4)
+                RETURNING ig_id AS "igId",
+                        caption,
+                        perm_url AS "permUrl",
+                        image_url AS imageUrl`,
+            [data.igId, data.caption, data.permUrl, data.imageUrl]
         );
         const post = result.rows[0];
 
         return post;
     }
 
-    static async get(ig_id) {
-        /** Get post by ig_id
+    static async get(igId) {
+        /** Get post by igId
          *
-         * Accepts ig_id
+         * Accepts igId
          *
-         * Returns { ig_id, caption, perm_url, image_url }
+         * Returns { igId, caption, permUrl, imageUrl }
          *
-         * Throws NotFoundError if post not found via ig_id
-         * Throws BadRequestError if no ig_id
+         * Throws NotFoundError if post not found via igId
+         * Throws BadRequestError if no igId
          */
         // check for missing input
-        if (!ig_id) throw new BadRequestError("No id provided.");
+        if (!igId) throw new BadRequestError("No id provided.");
 
-        // query db for igpost by ig_id
+        // query db for igpost by igId
         const result = await db.query(
-            `SELECT ig_id,
+            `SELECT ig_id AS "igId",
                     caption,
-                    perm_url,
-                    image_url
+                    perm_url AS "permUrl",
+                    image_url AS "imageUrl"
             FROM igposts
             WHERE ig_id = $1`,
-            [ig_id]
+            [igId]
         );
         const post = result.rows[0];
 
         // if no record returned, no igpost is found, throw NotFoundError
-        if (!post) throw new NotFoundError(`No post found for: ${ig_id}`);
+        if (!post) throw new NotFoundError(`No post found for: ${igId}`);
 
         return post;
     }
@@ -66,46 +72,46 @@ class IGPost {
     static async getAll() {
         /** Get all posts
          *
-         * Returns [{ ig_id, caption, perm_url, image_url },
-         *              { ig_id, caption, perm_url, image_url }, ...]
+         * Returns [{ igId, caption, permUrl, imageUrl },
+         *              { igId, caption, permUrl, imageUrl }, ...]
          */
         // query db for list of igposts
         const result = await db.query(
-            `SELECT ig_id,
+            `SELECT ig_id AS "igId",
                     caption,
-                    perm_url,
-                    image_url
+                    perm_url AS permUrl,
+                    image_url AS imageUrl
             FROM igposts`
         );
 
         return result.rows;
     }
 
-    static async delete(ig_id) {
-        /** Delete post by ig_id
+    static async delete(igId) {
+        /** Delete post by igId
          *
-         * Accepts ig_id
+         * Accepts igId
          *
          * Returns { msg: "Deleted." }
          *
-         * Throws NotFoundError if post not found via ig_id
+         * Throws NotFoundError if post not found via igId
          * Throw BadRequestError if no input
          */
         // check for missing input
-        if (!ig_id) throw new BadRequestError("No input.");
+        if (!igId) throw new BadRequestError("No input.");
 
         // query db to delete igpost
         const result = await db.query(
             `DELETE
             FROM igposts
             WHERE ig_id = $1
-            RETURNING ig_id`,
-            [ig_id]
+            RETURNING ig_id AS "igId"`,
+            [igId]
         );
         const removed = result.rows[0];
 
         // if no record returned, no igpost found, throw NotFoundError
-        if (!removed) throw new NotFoundError(`No post: ${ig_id}`);
+        if (!removed) throw new NotFoundError(`No post: ${igId}`);
 
         return { msg: "Deleted." };
     }

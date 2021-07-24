d3e333142762b9b25e0ed9d4dfa1f44ab622226f
booshja <jacob.andes@gmail.com>
2021-07-24 00:10:28 -0700

    feat: image uploads with items routes
    Added image upload for items routes, without tests
    
    References: PJ-37

--- routes/items.js
+++ routes/items.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const jsonschema = require("jsonschema");
-const { BadRequestError } = require("../expressError");
+const multer = require("multer");
+const { BadRequestError, NotFoundError } = require("../expressError");
 const Item = require("../models/item");
 const itemNewSchema = require("../schemas/itemNew.json");
 const itemUpdateSchema = require("../schemas/itemUpdate.json");
 
 const router = express.Router({ mergeParams: true });
+
+// multer options
+const upload = multer({
+    limits: {
+        // limits filesize to 1 megabyte
+        fileSize: 1000000,
+    },
+    fileFilter(req, file, cb) {
+        // restricts file types to png, jpg, jpeg
+        if (!file.originalname.match(/\.(png|jpg|jpeg)$/)) {
+            // callback to throw error if filetype not image
+            cb(new BadRequestError("Please upload an image."));
+        }
+        // callback to continue
+        cb(undefined, true);
+    },
+});
 
 router.post("/", async (req, res, next) => {
     /** POST "/" { item } => { item }
@@ -30,6 +48,31 @@ router.post("/", async (req, res, next) => {
         return next(err);
     }
 });
+
+router.post(
+    "/upload/:itemId",
+    upload.single("upload"),
+    async (req, res) => {
+        /** POST "/upload/{itemId}" { file upload } => { message }
+         * Upload an image and save as binary to db
+         *
+         * Returns { msg: "Upload successful." }
+         *
+         * Authorization Required: admin
+         **/
+        try {
+            const message = await Item.uploadImage(+req.params.itemId, {
+                image: req.file.buffer,
+            });
+            res.status(200).send({ message });
+        } catch (err) {
+            res.status(400).send(err);
+        }
+    },
+    (error, req, res, next) => {
+        res.status(400).send({ error: error.message });
+    }
+);
 
 router.get("/", async (req, res, next) => {
     /** GET "/" => [ items ]
@@ -63,6 +106,28 @@ router.get("/item/:id", async (req, res, next) => {
         return res.status(200).json({ item });
     } catch (err) {
         return next(err);
+    }
+});
+
+router.get("/item/:itemId/image", async (req, res) => {
+    /** GET "/item/:itemId/image" => image
+     * Returns the image associated with the item
+     *
+     * Returns image data
+     *
+     * Authoriation required: none
+     **/
+    try {
+        // get item image by id
+        const itemImage = await Item.getImage(req.params.itemId);
+        // if no item or no image in item, throw NotFoundError
+        if (!itemImage) throw new NotFoundError("No image found.");
+
+        //response header, use set
+        res.set("Content-Type", "image/png");
+        res.status(200).send(itemImage);
+    } catch (err) {
+        res.status(404).send(err);
     }
 });
 
@@ -111,6 +176,12 @@ router.patch("/update/:id", async (req, res, next) => {
      * Authorization required: admin
      */
     try {
+        const validator = jsonschema.validate(req.body, itemUpdateSchema);
+        if (!validator.valid) {
+            const errors = validator.errors.map((e) => e.stack);
+            throw new BadRequestError(errors);
+        }
+
         const item = await Item.update(+req.params.id, req.body);
         return res.status(200).json({ item });
     } catch (err) {
@@ -147,6 +218,22 @@ router.patch("/sold/:id", async (req, res, next) => {
         return res.status(200).json({ item });
     } catch (err) {
         return next(err);
+    }
+});
+
+router.delete("/item/:imageId/image", async (req, res) => {
+    /** DELETE "/upload" => undefined
+     * Deletes image data from an item
+     *
+     * Returns { msg: "Deleted." }
+     *
+     * Authorization required: admin
+     */
+    try {
+        const message = await Item.deleteImage(+req.params.imageId);
+        res.status(200).send({ message });
+    } catch (err) {
+        res.status(400).send(err);
     }
 });
 

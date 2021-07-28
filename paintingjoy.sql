75d444ae0e54ea747302e211ad98b7668925e6d6
booshja <jacob.andes@gmail.com>
2021-07-27 22:26:46 -0700

    refactor: removed unneccesary line of commented out code

--- paintingjoy.sql
+++ paintingjoy.sql
@@ -6,7 +6,6 @@ CREATE DATABASE paintingjoy;
 \connect paintingjoy
 
 \i paintingjoy-schema.sql
--- \i paintingjoy-seed.sql
 
 \echo 'Delete and recreate pj_test db?'
 \prompt 'Return for yes or Ctrl+C to cancel > ' foo
